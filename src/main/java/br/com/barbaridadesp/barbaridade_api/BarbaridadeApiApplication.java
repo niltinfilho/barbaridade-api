be2c75c217c389f51f6b7ed9d3d3da1ceef3d12d
niltinf <niltonfilho115@gmail.com>
2026-03-26 02:51:47 -0300

    first commit: create project

---
+++ src/main/java/br/com/barbaridadesp/barbaridade_api/BarbaridadeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.barbaridadesp.barbaridade_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbaridadeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbaridadeApiApplication.class, args);
+	}
+
+}
