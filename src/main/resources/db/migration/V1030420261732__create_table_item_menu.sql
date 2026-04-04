95263a05c39470505e8e1405ba845eee76f83689
niltinfilho <niltonfilho115@gmail.com>
2026-04-04 13:38:15 -0300

    feat: add db configurations and create tables

---
+++ src/main/resources/db/migration/V1030420261732__create_table_item_menu.sql
@@ -0,0 +1,13 @@
+-- create_table_item_menu
+
+CREATE TABLE IF NOT EXISTS item_menu (
+    id INT8 PRIMARY KEY NOT NULL,
+    uuid UUID NOT NULL,
+    base_64_img TEXT NOT NULL,
+    alt_description VARCHAR(255) NOT NULL,
+    nome VARCHAR(155) NOT NULL,
+    descricao VARCHAR(155) NOT NULL,
+    categoria VARCHAR(30) NOT NULL,
+    ativo BOOL NOT NULL,
+    data_criacao TIMESTAMP NOT NULL
+);
